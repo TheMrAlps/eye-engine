4eb4206f42c22539503fab89c17bfcc0996fd834
TheMrAlps <mark@newvieweyecaremi.com>
2026-07-19 22:59:57 -0400

    Add Anatomy class with eye parts initialization

--- engine/anatomy/Anatomy.js
+++ engine/anatomy/Anatomy.js
@@ -1,1 +1,43 @@
+import Sclera from "./Sclera.js";
+import Cornea from "./Cornea.js";
+import Iris from "./Iris.js";
+import Pupil from "./Pupil.js";
+import Lens from "./Lens.js";
 
+export default class Anatomy {
+
+    constructor(svg){
+
+        this.parts = [
+
+            new Sclera(),
+
+            new Cornea(),
+
+            new Iris(),
+
+            new Pupil(),
+
+            new Lens()
+
+        ];
+
+        this.parts.forEach(
+
+            part => part.appendTo(svg)
+
+        );
+
+    }
+
+    update(model){
+
+        this.parts.forEach(
+
+            part => part.update(model)
+
+        );
+
+    }
+
+}

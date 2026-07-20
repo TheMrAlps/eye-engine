674e0eba67421af7a9926fad82b68292e2cba118
TheMrAlps <mark@newvieweyecaremi.com>
2026-07-19 23:00:23 -0400

    Import EyeModel and Anatomy in Eye class
    Refactor Eye class to include EyeModel and Anatomy imports.

--- engine/Eye.js
+++ engine/Eye.js
@@ -1,8 +1,19 @@
-class Eye {
+import EyeModel from "./EyeModel.js";
+import Anatomy from "./anatomy/Anatomy.js";
+
+export default class Eye {
 
     constructor(svg){
 
-        this.svg = svg;
+        this.model = new EyeModel();
+
+        this.anatomy = new Anatomy(svg);
+
+    }
+
+    draw(){
+
+        this.anatomy.update(this.model);
 
     }
 

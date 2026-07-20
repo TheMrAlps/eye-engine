0c0419999d08ab397060722d9d5b2f0ada097a57
TheMrAlps <mark@newvieweyecaremi.com>
2026-07-19 23:09:11 -0400

    Update Drawable class constructor and methods
    Refactor Drawable class to accept SVG parent and remove unused methods.

--- engine/anatomy/Drawable.js
+++ engine/anatomy/Drawable.js
@@ -1,90 +1,21 @@
-/* ==========================================================
-   Eye Engine
-
-   Drawable.js
-
-   Base class for every drawable SVG object.
-
-========================================================== */
-
 import SVG from "../utils/SVG.js";
 
 export default class Drawable {
 
-    constructor(tag = "g") {
+    constructor(svg, tag){
 
         this.element = SVG.create(tag);
 
-        this.visible = true;
+        svg.appendChild(this.element);
 
     }
 
-    /**
-     * Returns the SVG element.
-     */
-    getElement() {
-
-        return this.element;
-
-    }
-
-    /**
-     * Append to an SVG parent.
-     */
-    appendTo(parent) {
-
-        parent.appendChild(this.element);
-
-    }
-
-    /**
-     * Update SVG attributes.
-     */
-    set(attributes = {}) {
+    set(attributes){
 
         SVG.set(this.element, attributes);
 
     }
 
-    /**
-     * Show element.
-     */
-    show() {
-
-        this.visible = true;
-
-        this.element.style.display = "";
-
-    }
-
-    /**
-     * Hide element.
-     */
-    hide() {
-
-        this.visible = false;
-
-        this.element.style.display = "none";
-
-    }
-
-    /**
-     * Remove from DOM.
-     */
-    destroy() {
-
-        this.element.remove();
-
-    }
-
-    /**
-     * Override in subclasses.
-     */
-    draw(model) {}
-
-    /**
-     * Override in subclasses.
-     */
-    update(model) {}
+    update(model){}
 
 }

72ffdfef9452e57487f93a6256ec1004780fb1fd
TheMrAlps <mark@newvieweyecaremi.com>
2026-07-19 22:58:43 -0400

    Implement Cornea class extending Drawable

--- engine/anatomy/Cornea.js
+++ engine/anatomy/Cornea.js
@@ -1,1 +1,47 @@
+import Drawable from "./Drawable.js";
+import Theme from "../../config/Theme.js";
 
+export default class Cornea extends Drawable {
+
+    constructor() {
+
+        super("path");
+
+        this.set({
+
+            fill: "none",
+
+            stroke: Theme.colors.blue,
+
+            "stroke-width": Theme.strokes.cornea
+
+        });
+
+    }
+
+    update(model) {
+
+        const x = model.sclera.front;
+
+        const y = model.center.y;
+
+        const r = model.cornea.radius;
+
+        const p = model.cornea.protrusion;
+
+        const d = `
+
+            M ${x} ${y}
+
+            C
+            ${x+p} ${y-r}
+            ${x+p} ${y+r}
+            ${x} ${y}
+
+        `;
+
+        this.set({ d });
+
+    }
+
+}

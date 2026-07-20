e21a257b4c81946d99b2bce66bc3afa68e18282b
TheMrAlps <mark@newvieweyecaremi.com>
2026-07-19 22:59:17 -0400

    Add Pupil class extending Drawable with update method

--- engine/anatomy/Pupil.js
+++ engine/anatomy/Pupil.js
@@ -1,1 +1,34 @@
+import Drawable from "./Drawable.js";
+import Theme from "../../config/Theme.js";
 
+export default class Pupil extends Drawable {
+
+    constructor(){
+
+        super("ellipse");
+
+        this.set({
+
+            fill:Theme.colors.navy
+
+        });
+
+    }
+
+    update(model){
+
+        this.set({
+
+            cx:model.anchors.iris,
+
+            cy:model.center.y,
+
+            rx:model.pupil.radiusX,
+
+            ry:model.pupil.radiusY
+
+        });
+
+    }
+
+}

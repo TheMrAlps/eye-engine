9c6b8fe8f41dd30e48b1813697eb22e1775a4b11
TheMrAlps <mark@newvieweyecaremi.com>
2026-07-19 22:59:00 -0400

    Implement Iris class extending Drawable

--- engine/anatomy/Iris.js
+++ engine/anatomy/Iris.js
@@ -1,1 +1,38 @@
+import Drawable from "./Drawable.js";
+import Theme from "../../config/Theme.js";
 
+export default class Iris extends Drawable {
+
+    constructor(){
+
+        super("ellipse");
+
+        this.set({
+
+            fill:"#7FB3D5",
+
+            stroke:Theme.colors.navy,
+
+            "stroke-width":4
+
+        });
+
+    }
+
+    update(model){
+
+        this.set({
+
+            cx:model.anchors.iris,
+
+            cy:model.center.y,
+
+            rx:model.iris.radiusX,
+
+            ry:model.iris.radiusY
+
+        });
+
+    }
+
+}

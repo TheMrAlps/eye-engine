091160dc2e962b1d600446afc2de70e5c6665c26
TheMrAlps <mark@newvieweyecaremi.com>
2026-07-19 22:31:26 -0400

    Update SVG.js

--- engine/utils/SVG.js
+++ engine/utils/SVG.js
@@ -1,1 +1,32 @@
+// engine/utils/SVG.js
 
+const SVG_NS = "http://www.w3.org/2000/svg";
+
+export default class SVG {
+
+    static create(tag, attributes = {}) {
+
+        const element =
+            document.createElementNS(SVG_NS, tag);
+
+        Object.entries(attributes).forEach(
+            ([key, value]) => {
+                element.setAttribute(key, value);
+            }
+        );
+
+        return element;
+
+    }
+
+    static set(element, attributes = {}) {
+
+        Object.entries(attributes).forEach(
+            ([key, value]) => {
+                element.setAttribute(key, value);
+            }
+        );
+
+    }
+
+}
